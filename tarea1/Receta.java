2b422b0b2be20785d833e0ccc7780677e8dd3595
Mawi <Mawi3003@hotmail.com>
2017-05-18 16:37:03 +0200

    Añadidos getters and setters y haciendo  private los atributos de la clase
    Signed-off-by: Mawi <Mawi3003@hotmail.com>

--- tarea1/Receta.java
+++ tarea1/Receta.java
@@ -2,9 +2,9 @@ package tarea1;
 
 public class Receta {
 
-	String nombre;
-	String ingredientes;
-	String preparacion;
+	private String nombre;
+	private String ingredientes;
+	private String preparacion;
 	
 	public Receta() {
 		
@@ -14,6 +14,30 @@ public class Receta {
 	
 		this.nombre = nombre;
 		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public String getPreparacion() {
+		return preparacion;
+	}
+
+	public void setPreparacion(String preparacion) {
 		this.preparacion = preparacion;
 	}
 }
