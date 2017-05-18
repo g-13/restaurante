c11dd4bec2f555e01d98c551d292dcfc1da2a24c
Mawi <Mawi3003@hotmail.com>
2017-05-18 16:34:01 +0200

    Agregacion de los 2 constructores
    Signed-off-by: Mawi <Mawi3003@hotmail.com>

--- tarea1/Receta.java
+++ tarea1/Receta.java
@@ -1,7 +1,19 @@
 package tarea1;
 
 public class Receta {
+
 	String nombre;
 	String ingredientes;
 	String preparacion;
+	
+	public Receta() {
+		
+	}
+
+	public Receta(String nombre, String ingredientes, String preparacion) {
+	
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
 }
