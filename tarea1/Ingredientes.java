f775a910a0ad8f1308ef492d21d1fd744918d97f
Mawi <Mawi3003@hotmail.com>
2017-05-18 16:56:35 +0200

    Agregado Constructores y getters and setters
    Signed-off-by: Mawi <Mawi3003@hotmail.com>

--- tarea1/Ingredientes.java
+++ tarea1/Ingredientes.java
@@ -1,6 +1,31 @@
 package tarea1;
 
-public final class Ingredientes {
+public class Ingredientes {
 	private String nombre;
 	private String cantidad;
+	
+	public Ingredientes() {
+		
+	}
+
+	public Ingredientes(String nombre, String cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
 }
